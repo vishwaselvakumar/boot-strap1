e3928a1179d59309a280b8114cd3403775689794
Vishwa S <110627130+vishwaselvakumar@users.noreply.github.com>
2023-11-20 15:58:49 +0000

    Add files via upload

---
+++ boot/script.js
@@ -0,0 +1,35 @@
+(function ($) {
+    $(function () {
+  
+      /* category carousel */
+      $('.js-category-carousel').slick({
+        infinite: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '.js-ag-carousel-arrow_prev',
+        nextArrow: '.js-ag-carousel-arrow_next',
+  
+        variableWidth: true,
+        centerPadding: '100px',
+  
+        responsive: [{
+          breakpoint: 981,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }]
+      });
+      /* /category carousel */
+  
+    });
+  })(jQuery);
